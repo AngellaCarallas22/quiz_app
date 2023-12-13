16258a23ec8b812aaf6a0af7e0239a8b9e759720
AngellaCarallas22 <148545769+AngellaCarallas22@users.noreply.github.com>
2023-12-13 20:58:48 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,209 @@
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 16;
+let countdown;
+
+//Questions and Options array
+
+const quizArray = [
+  {
+    id: "0",
+    question: "What does the acronym API stand for?",
+    options: [
+      "Application Programming Interface",
+      "Advanced Programming Interface",
+      "Automated Program Integration",
+      "Application Protocol Interface",
+    ],
+    correct: "Application Programming Interface",
+  },
+  {
+    id: "1",
+    question: "What is the purpose of the CSS box model?",
+    options: [
+      "To create 3D effects on web pages",
+      "To control the layout and spacing of elements",
+      "To define color schemes for websites",
+      "To manage server-side data",
+    ],
+    correct: "To control the layout and spacing of elements",
+  },
+  {
+    id: "2",
+    question: "What is the purpose of the HTML <meta> tag?",
+    options: [
+      "To define metadata about the document",
+      "To create hyperlinks",
+      "To embed external scripts",
+      "To define the main content of the document",
+    ],
+    correct: "To define metadata about the document",
+  },
+  {
+    id: "3",
+    question:
+      "What do you call a computer on a network that requests files from another computer?",
+    options: ["A client", "A host", "A router", "A web server"],
+    correct: "A client",
+  },
+  {
+    id: "4",
+    question:
+      "What does the term responsive design refer to in web development?",
+    options: [
+      "Designing websites that respond to user input",
+      "Creating visually appealing websites",
+      "Designing websites that adapt to different screen sizes",
+      "Designing websites with a fast response time",
+    ],
+    correct: "Designing websites that adapt to different screen sizes",
+  },
+];
+
+//Restart Quiz
+restart.addEventListener("click", () => {
+  initial();
+  displayContainer.classList.remove("hide");
+  scoreContainer.classList.add("hide");
+});
+
+//Next Button
+nextBtn.addEventListener(
+  "click",
+  (displayNext = () => {
+    questionCount += 1;
+    //if last question
+    if (questionCount == quizArray.length) {
+      //hide question container and display score
+      displayContainer.classList.add("hide");
+      scoreContainer.classList.remove("hide");
+      //user score
+      userScore.innerHTML =
+        "Your score is " + scoreCount + " out of " + questionCount;
+    } else {
+      countOfQuestion.innerHTML =
+        questionCount + 1 + " of " + quizArray.length + " Question";
+      //display quiz
+      quizDisplay(questionCount);
+      count = 16;
+      clearInterval(countdown);
+      timerDisplay();
+    }
+  })
+);
+
+//Timer
+const timerDisplay = () => {
+  countdown = setInterval(() => {
+    count--;
+    timeLeft.innerHTML = `${count}s`;
+    if (count == 0) {
+      clearInterval(countdown);
+      displayNext();
+    }
+  }, 1000);
+};
+
+//Display quiz
+const quizDisplay = (questionCount) => {
+  let quizCards = document.querySelectorAll(".container-mid");
+  //Hide other cards
+  quizCards.forEach((card) => {
+    card.classList.add("hide");
+  });
+  //display current question card
+  quizCards[questionCount].classList.remove("hide");
+};
+
+//Quiz Creation
+function quizCreator() {
+  //randomly sort questions
+  quizArray.sort(() => Math.random() - 0.5);
+  //generate quiz
+  for (let i of quizArray) {
+    //randomly sorting of a questions
+    i.options.sort(() => Math.random() - 0.5);
+
+    let div = document.createElement("div");
+    div.classList.add("container-mid", "hide");
+    //question number
+    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+    //question
+    let question_DIV = document.createElement("p");
+    question_DIV.classList.add("question");
+    question_DIV.innerHTML = i.question;
+    div.appendChild(question_DIV);
+    //options
+    div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+    <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+    <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+    <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+    quizContainer.appendChild(div);
+  }
+}
+
+//Checker Function to check if option is correct or not
+function checker(userOption) {
+  let userSolution = userOption.innerText;
+  let question =
+    document.getElementsByClassName("container-mid")[questionCount];
+  let options = question.querySelectorAll(".option-div");
+
+  //if user clicked answer == correct option stored in object
+  if (userSolution === quizArray[questionCount].correct) {
+    userOption.classList.add("correct");
+    scoreCount++;
+  } else {
+    userOption.classList.add("incorrect");
+    //For marking the correct option
+    options.forEach((element) => {
+      if (element.innerText == quizArray[questionCount].correct) {
+        element.classList.add("correct");
+      }
+    });
+  }
+
+  //clear interval(stop timer)
+  clearInterval(countdown);
+  //disable all options
+  options.forEach((element) => {
+    element.disabled = true;
+  });
+}
+
+//initial setup
+function initial() {
+  quizContainer.innerHTML = "";
+  questionCount = 0;
+  scoreCount = 0;
+  count = 16;
+  clearInterval(countdown);
+  timerDisplay();
+  quizCreator();
+  quizDisplay(questionCount);
+}
+
+//when user click on start button
+startButton.addEventListener("click", () => {
+  startScreen.classList.add("hide");
+  displayContainer.classList.remove("hide");
+  initial();
+});
+
+//hide quiz and display start screen
+window.onload = () => {
+  startScreen.classList.remove("hide");
+  displayContainer.classList.add("hide");
+};
